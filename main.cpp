ea5c1c44a6bf2ec932fd737704e6b615d98b6b33
AndresEd-sant <andreseduardo.santos@udea.edu.co>
2025-05-15 14:45:30 -0500

    COMMIT #2 Estructura main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,52 @@
+// src/main.cpp
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    cout << "Programa UdeaStay !" << endl;
+void menuAdministrador() {
+    cout << "\n=== MENÚ ADMINISTRADOR ===\n";
+    cout << "1. Cambio de Dia\n";
+    cout << "2. Consultar Huespedes\n";
+    cout << "3. Salir\n";
+}
+
+void menuUsuario() {
+    cout << "\n=== MENÚ HUÉSPED ===\n";
+    cout << "1. Buscar alojamiento\n";
+    cout << "2. Ver reservaciones\n";
+    cout << "3. Cancelacion de reservas\n";
+    cout << "4. Salir\n";
+}
+
+string login(string *documento, string *contrasena){
+    string tipo;
+    //Prototipo de autenticacion
+    return tipo;
+}
+
+int main() {
+    string documento;
+    string contrasena;
+
+    cout << "Bienvenido a UdeAStay\n";
+    cout << "Ingrese su numero de documento: ";
+    cin >> documento;
+    cout << "Ingrese su contrasena: ";
+    cin >> contrasena;
+
+
+
+    // Boceto de autenticación (simulada)
+    if (login(&documento,&contrasena) == "admin") {
+        cout << "\nInicio de sesión exitoso como ADMINISTRADOR\n";
+        menuAdministrador();
+    } else if (login(&documento,&contrasena) == "usuario") {
+        cout << "\nInicio de sesión exitoso como HUÉSPED\n";
+        menuUsuario();
+    } else {
+        cout << "\nTipo de usuario no válido. Intente de nuevo.\n";
+    }
+
     return 0;
 }
